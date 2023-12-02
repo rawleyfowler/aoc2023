73c19f321c6eaefe886a6df067b2067906add65f
Rawley <rawleyfowler@proton.me>
2023-12-01 23:40:23 -0600

    Day 2

---
+++ day2.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use DDP;
+use List::Util qw(max min);
+
+my $sum;
+my $power;
+while(<STDIN>) {
+  chomp;
+  my @id = /Game\s(\d+):.*/gx;
+  my @red = /(\d+)\sred/gmx;
+  my @grn = /(\d+)\sgreen/gmx;
+  my @blu = /(\d+)\sblue/gmx;
+
+  my $mred = max(@red);
+  my $mgrn = max(@grn);
+  my $mblu = max(@blu);
+
+  $power += ($mred * $mgrn * $mblu);
+      
+  next unless ($mred <= 12 &&
+               $mgrn <= 13 &&
+               $mblu <= 14);
+               
+  $sum += pop @id;
+}
+print "PART 1: $sum\n";
+print "PART 2: $power\n";
