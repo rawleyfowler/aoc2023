9cae79613267c5881131c089fbf46b1cc5ce76c9
Rawley <rawleyfowler@proton.me>
2023-12-07 10:35:45 -0600

    more day 5 wtfs

--- day5.pl
+++ day5.pl
@@ -8,7 +8,7 @@ use DDP;
 use feature qw(state say);
 
 my @filters;
-my $input = join( "", <STDIN> );
+chomp( my $input = join( "", <STDIN> ) );
 my @maps  = split( /\n\n/x, $input );
 my @seeds = ( shift(@maps) =~ /(\d+)/gmx );
 
@@ -29,9 +29,6 @@ for (@maps) {
             my ( $d, $s, $k ) = @$_;
 
             my $sdelta = $s + ( $k - 1 );
-            my $ddelta = $d + $k;
-
-            #say "$d $s $k -- $val";
 
             if ( $val <= $sdelta && $val >= $s ) {
                 $val = $d + ( abs( $val - $s ) );
@@ -61,65 +58,96 @@ my @r = map { do_mapping($_) } @seeds;
 print "PART 1: ", min(@r), "\n\n\n\n\n";
 
 sub range {
-    return +{ hi => shift, lo => shift };
-}
-
-my @p2_filters;
-for (@maps) {
-    my @split = ( split /\n/, $_ );
-    my @nums  = map { [/(\d+)/gx] } @split[ 1 .. $#split ];
-
-    my $filter = sub {
-        my $highlows = shift;
-
-        my @ret;
-        for (@nums) {
-            my ( $d, $s, $k ) = @$_;
-
-            my $delta  = $d - $s - 1;
-            my $sdelta = $s + ( $k - 1 );
-
-            while ( my $o = pop $highlows->@* ) {
-                my $high = $o->{hi};
-                my $low  = $o->{lo};
-
-                if ( max( $s, $low ) - min( $sdelta, $high ) > 0 ) {
-                    push @ret, range( $high, $low );
-                }
-                elsif ( $low >= $s && $high <= $sdelta ) {
-                    push @ret, range( $high + $delta, $low + $delta );
-                }
-                elsif ( $low < $s && $high > $sdelta ) {
-                    push @ret,          range( $s + $delta, $sdelta + $delta );
-                    push $highlows->@*, range( $low,        $s );
-                    push $highlows->@*, range( $sdelta,     $high );
-                }
-                elsif ( $low < $s && $high < $sdelta ) {
-                    push @ret,          range( $s + $delta, $high + $delta );
-                    push $highlows->@*, range( $low,        $s );
-                }
-                elsif ( $low > $s && $high > $sdelta ) {
-                    push @ret, range( $low + $delta, $sdelta + $delta );
-                    push $highlows->@*, range( $sdelta, $high );
-                }
-            }
-        }
-
-        return \@ret;
-    };
-
-    push @p2_filters, $filter;
+    my ( $low, $high ) = @_;
+    return +{ lo => $low, hi => $high };
 }
 
 my %seed_ranges_map = @seeds;
-my @m = map { +{ lo => $_, hi => $_ + ( $seed_ranges_map{$_} - 1 ) } }
+my @highlows        = map { range( $_, $_ + $seed_ranges_map{$_} - 1 ) }
   keys(%seed_ranges_map);
 
-my $m = \@m;
-for (@p2_filters) {
-    $m = $_->($m);
+for (@maps) {
+    my @split = ( split /\n/, $_ );
+    my @nums  = map { [/(\d+)/gx] } @split[ 1 .. $#split ];
+    my @ret;
+    for (@nums) {
+        my ( $d, $s, $k ) = @$_;
+
+        my $delta = sub {
+            return shift() + ( $d - $s );
+        };
+
+        say "$d $s $k";
+
+        my $start = $s;
+        my $end   = $s + $k - 1;
+        my @next;
+        while ( my $o = pop @highlows ) {
+
+            my $high = $o->{hi};
+            my $low  = $o->{lo};
+
+            # No overlap
+            #
+            # XXX
+            #     YYYYYYY
+            if ( ( max( $low, $start ) - min( $high, $end ) ) > 0 ) {
+                print "NO OVERLAP: $low, $high, $start, $end\n";
+                push @next, range( $low, $high );
+            }
+
+            # Inclusive overlap
+            #
+            #    XXXXXXX
+            #   YYYYYYYYY
+            elsif ($low >= $start
+                && $low < $end
+                && $high <= $end
+                && $high > $start )
+            {
+                print "INCLUSIVE OVERLAP: $low, $high, $start, $end\n";
+                push @ret, range( $delta->($low), $delta->($high) );
+            }
+
+            # Double sided Overlap
+            #
+            #  XXXXXXXXXXX
+            #    YYYYYYY
+            elsif ( $low < $start && $high > $end ) {
+                print "DOUBLE SIDED OVERLAP: $low, $high, $start, $end\n";
+                push @ret,  range( $delta->($start), $delta->($end) );
+                push @next, range( $start - 1,       $low );
+                push @next, range( $end + 1,         $high );
+            }
+
+            # Left Overlap
+            #
+            #  XXXXXX
+            #    YYYYYYY
+            elsif ( $low < $start && $high <= $end && $high > $start ) {
+                print "LEFT OVERLAP: $low, $high, $start, $end\n";
+                push @ret,  range( $delta->($start), $delta->($high) );
+                push @next, range( $low,             $start - 1 );
+            }
+
+            # Right Overlap
+            #
+            #      XXXXXXX
+            #    YYYYYYY
+            elsif ( $low >= $start && $high > $end && $low < $end ) {
+                print "RIGHT OVERLAP: $low, $high, $start, $end\n";
+                push @ret,  range( $delta->($low), $delta->($end) );
+                push @next, range( $end + 1,       $high );
+            }
+            else {
+                say "WTF: $low, $high | $start, $end";
+            }
+        }
+
+        @highlows = ( @next, @ret );
+        p @highlows;
+        @ret = ();
+    }
 }
 
-# p $m;
-
-print "PART 2: ", min( map { $_->{lo}, $_->{hi} } $m->@* ), "\n";
+print "PART 2: ", min( map { $_->{lo}, $_->{hi} } @highlows ), "\n";
